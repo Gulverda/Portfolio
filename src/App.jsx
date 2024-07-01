0a8e8a8b56dc3bcc2ecc01274338dc832a984eda
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-07-02 03:18:18 +0400

    Added Web And Game Project cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,38 @@ import React from 'react';
 import Hero from '../src/components/main/hero.jsx';
 import Navbar from '../src/components/main/navbar.jsx';
 import RootLayout from './layout.jsx';
+import Card from './components/Cards/Card.jsx';
+import ProductsIcon from './components/Cards/ProductsIcon.jsx';
+import CategoriesIcon from './components/Cards/CategoriesIcon.jsx';
 import { StarsCanvas } from '../src/components/main/startBg.jsx';
 
 
 const App = () => {
   return (
-    <RootLayout>
+    <>
+        <RootLayout>
 
-         <div>
-         {/* <StarsCanvas /> */}
+<div>
+{/* <StarsCanvas /> */}
 
-          <Navbar />
-            <Hero />
-          {/* <h1>Welcome to my React App!</h1> */}
-        </div>
-    </RootLayout>
+ <Navbar />
+   <Hero />
+ {/* <h1>Welcome to my React App!</h1> */}
+</div>
+</RootLayout>
+<div className="grid">
+<Card
+icon={<ProductsIcon />}
+title="Web Application"
+description="Standard chunk of Lorem Ipsum used since the 1500s is showed below for those interested."
+/>
+<Card
+icon={<CategoriesIcon />}
+title="Games"
+description="Standard chunk of Lorem Ipsum used since the 1500s is showed below for those interested."
+/>
+</div>
+</>
   );
 };
 
---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../../CSS/Card.css'; // Assuming you have a CSS file for styles
+
+const Card = ({ icon, title, description }) => (
+  <div className="card">
+    <span className="icon">{icon}</span>
+    <h4>{title}</h4>
+    <p>{description}</p>
+    <div className="shine"></div>
+    <div className="background">
+      <div className="lines">
+        <div className="line line-1"></div>
+        <div className="line line-2"></div>
+        <div className="line line-3"></div>
+      </div>
+      <div className="tiles">
+        <div className="tile tile-1" style={{ "--fromX": "0px", "--fromY": "0px", "--toX": "100px", "--toY": "100px" }}></div>
+        <div className="tile tile-2" style={{ "--fromX": "10px", "--fromY": "20px", "--toX": "90px", "--toY": "80px" }}></div>
+        <div className="tile tile-3" style={{ "--fromX": "20px", "--fromY": "40px", "--toX": "80px", "--toY": "60px" }}></div>
+        <div className="tile tile-4" style={{ "--fromX": "30px", "--fromY": "60px", "--toX": "70px", "--toY": "40px" }}></div>
+        <div className="tile tile-5" style={{ "--fromX": "40px", "--fromY": "80px", "--toX": "60px", "--toY": "20px" }}></div>
+        <div className="tile tile-6" style={{ "--fromX": "50px", "--fromY": "100px", "--toX": "50px", "--toY": "0px" }}></div>
+        <div className="tile tile-7" style={{ "--fromX": "60px", "--fromY": "120px", "--toX": "40px", "--toY": "-20px" }}></div>
+        <div className="tile tile-8" style={{ "--fromX": "70px", "--fromY": "140px", "--toX": "30px", "--toY": "-40px" }}></div>
+        <div className="tile tile-9" style={{ "--fromX": "80px", "--fromY": "160px", "--toX": "20px", "--toY": "-60px" }}></div>
+        <div className="tile tile-10" style={{ "--fromX": "90px", "--fromY": "180px", "--toX": "10px", "--toY": "-80px" }}></div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Card;
