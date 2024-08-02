4eabb6fb9cca476cc9fc3083aa26629861de524b
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-08-02 11:40:07 +0400

    Fixed unused file/folder

--- src/components/Earth/SectionBanner.jsx
+++ src/components/Earth/SectionBanner.jsx
@@ -1,14 +1,14 @@
 // SectionBanner.jsx
 import React from 'react';
-import CurvedCornerStar from './CurvedCornerStar';
+// import CurvedCornerStar from './CurvedCornerStar';
 import '../../CSS/Earth.css'; // Import the CSS file
 
 const SectionBanner = () => (
   <div className="section-banner">
     {[...Array(7)].map((_, index) => (
       <div key={index + 1} id={`star-${index + 1}`}>
-        <CurvedCornerStar />
-        <CurvedCornerStar />
+        {/* <CurvedCornerStar />
+        <CurvedCornerStar /> */}
       </div>
     ))}
   </div>
