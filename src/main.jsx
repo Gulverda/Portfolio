3d4328abd5fad46671a192b5d6f8a82524b37130
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-08-10 22:45:01 +0400

    Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { StarsCanvas } from '../src/components/main/startBg.jsx'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { StarsCanvas } from '../src/components/main/startBg.jsx';
+
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Analytics />
-    <SpeedInsights />
     <StarsCanvas />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
