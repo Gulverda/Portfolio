0d9b1e84db0b0d4652e1da5987ab2bb9c0aaa6ed
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-08-10 22:43:01 +0400

    Added Vercel Features

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { StarsCanvas } from '../src/components/main/startBg.jsx';
+import { StarsCanvas } from '../src/components/main/startBg.jsx'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics />
+    <SpeedInsights />
     <StarsCanvas />
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
