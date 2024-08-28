11937d2af5e70f5d1e7cc2335722c4a6750f38a8
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-08-29 02:12:27 +0400

    Fixed Some BUgs

--- src/components/main/hero.jsx
+++ src/components/main/hero.jsx
@@ -5,15 +5,6 @@ const Hero = () => {
   return (
     <div style={{display: 'flex', justifyContent: 'center'}}>
       <div style={{ position: 'relative', display: 'flex', flexDirection: 'column', height: '100%', minHeight: '800px', width: '100%', maxWidth: '1440px' }}>
-      {/* <video
-        autoPlay
-        muted
-        loop
-        style={{ position: 'absolute', top: '-340px', left: '0', width: '100%', height: '100%', objectFit: 'cover', zIndex: '0', rotate: '180deg',}}
-      >
-        <source src={video1} type="video/webm" />
-      </video> */}
-
       <div style={{position: 'relative', display: 'flex', width: '100%', justifyContent: 'space-around'}}>
       <HeroContent />
       </div>
